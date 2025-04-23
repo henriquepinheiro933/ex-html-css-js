78e2cd1fcb52e0d73500728062135c61404fad32
henriquepinheiro933 <henriquepinheiro6666@gmail.com>
2025-04-22 21:20:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let date = new date()
+let horaAtual = date.gedhours()
+let minutoatual = date.getminutes()
+let diassemana = ["dom", "seg", 'ter', "qua", "qui", "sex", "sab"]
+let diaatualsemana = diassemana[date.getday()]
+let diaatualmes = date.getdate()
+let mesano = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+let mesatual = mesano [date.getmonth()]
+
+console.log()
